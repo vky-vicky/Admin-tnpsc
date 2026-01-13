5b25069093f7e3b3e5c4de60e03542b1aea2e98c
Vignesh <vignesh@ondrutech.com>
2026-01-13 16:37:19 +0530

    Fix mixed content issue using Vercel rewrites and Vite proxy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://35.172.178.163:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
